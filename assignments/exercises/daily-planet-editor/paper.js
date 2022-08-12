96342af91584de6795c51ed90b3b14c83956fbbc
Michael Hardin <hardin0419@gmail.com>
2022-08-12 10:19:08 -0600

    Formatted and tested paper.js

--- assignments/exercises/daily-planet-editor/paper.js
+++ assignments/exercises/daily-planet-editor/paper.js
@@ -6,52 +6,54 @@ In short, you shouldn't need to add your own statements anywhere,
 just fix the existing ones.
 */
 
-
 // Written by Kent, Clark
 
-var enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
+var enemies = [
+  "Lex",
+  "Batman",
+  "Darkseid",
+  "Brainiac",
+  "General Zod",
+  "Doomsday",
+];
 
-function whoWins(isThereKryptonite, enemyName)
-{
-if (!isThereKryptonite)
-{
+function whoWins(isThereKryptonite, enemyName) {
+  if (!isThereKryptonite) {
     return "Superman beats " + enemyName + ", of course";
+  } else {
+    return ("Depends on how quick Superman can get rid of the Kryptonite. " + enemyName + " could possibly win this one.");
+  }
 }
 
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. " + enemyName + " could possibly win this one.";
-}
-    }
+for (var i = 0; i < enemies.length; i++) {
+  var isThereKryptonite;
 
-for (var i = 0; i < enemies.length; i++)
-{
-    var isThereKryptonite;
-
-    if (i % 2 === 0) {
-        isThereKryptonite = true;
-    } else {
-        isThereKryptonite = false;
-    }
-    console.log(whoWins(isThereKryptonite, enemies[i]));
+  if (i % 2 === 0) {
+    isThereKryptonite = true;
+  } else {
+    isThereKryptonite = false;
+  }
+  console.log(whoWins(isThereKryptonite, enemies[i]));
 }
 
-function HowAttractedIsLoisLaneToMe ()
-{
-    // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Math.random() * 10) + 1);
+function HowAttractedIsLoisLaneToMe() {
+  // 1 is not at all attracted, 10 is "super" attracted...
+  return Math.floor(Math.random() * 10 + 1);
 }
 
-console.log(HowAttractedIsLoisLaneToMe()) ;
+console.log(HowAttractedIsLoisLaneToMe());
 
 var clarkKent = true;
 var superman = false;
 
 while (clarkKent) {
-    console.log("I'm just a nerdy columnist");
-    var phoneBoothQuickChange =Math.random();
-    if (phoneBoothQuickChange>= 0.5) {
-        clarkKent = false;
-        superman = true;
-        console.log("Now I'm Superman!");
-    }
-    }
+  console.log("I'm just a nerdy columnist");
+
+  var phoneBoothQuickChange = Math.random();
+
+  if (phoneBoothQuickChange >= 0.5) {
+    clarkKent = false;
+    superman = true;
+    console.log("Now I'm Superman!");
+  }
+}
