31c58e52836bd74ebb3284a07e757f20a6943ce2
Michael Hardin <hardin0419@gmail.com>
2022-08-15 13:16:16 -0600

    Added styles.css; edited index.js in Creating-Elements-JS directory

--- assignments/Level-1/DOM/Creating-Elements-JS/index.js
+++ assignments/Level-1/DOM/Creating-Elements-JS/index.js
@@ -1,13 +1,27 @@
 let newElement = document.createElement("li");
-newElement.textContent = "3";
+newElement.textContent = "3 // appended ↖︎";
 //var tempElement = "";
 
 document.querySelector("#my-list").appendChild(newElement);
 
 for (var i = 4; i < 10; i++) {
     var tempElement = document.createElement("li");
-    tempElement.textContent = i;
+    tempElement.textContent = i + " // loop'd ↖︎";
     document.querySelector("#my-list").appendChild(tempElement);
 
     console.log("Added ", tempElement, ".");
 }
+
+/**
+ * Challenge:
+ * 
+ * Add a paragraph after the existing unordered list with any text you want inside.
+ * Do this completely in JavaScript!
+ */
+
+let newParagraph = document.createElement("p");
+newParagraph.textContent = "This is a new paragraph!";
+
+newParagraph.classList.add("paragraphStyle");
+
+document.body.appendChild(newParagraph);
