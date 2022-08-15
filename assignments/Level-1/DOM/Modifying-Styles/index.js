489e4a6c81b25f74e7e9c7a823ce3ac023b94493
Michael Hardin <hardin0419@gmail.com>
2022-08-15 11:39:20 -0600

    Updated index.html, index.js in Modifying-Styles directory

---
+++ assignments/Level-1/DOM/Modifying-Styles/index.js
@@ -0,0 +1,7 @@
+var headerText = document.querySelector("#header");
+
+console.log(Object.keys(headerText.style));
+
+/*document.querySelector("body").style.backgroundColor = "#98e8fa";*/
+
+document.body.style.backgroundColor = "#98e8fa";
