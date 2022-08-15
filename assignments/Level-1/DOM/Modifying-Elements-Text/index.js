ff543e219c8ba28d7bfc29836d773e1637fa59e0
Michael Hardin <hardin0419@gmail.com>
2022-08-15 11:22:05 -0600

    Updated index.js in Modifying-Elements-Text directory

--- assignments/Level-1/DOM/Modifying-Elements-Text/index.js
+++ assignments/Level-1/DOM/Modifying-Elements-Text/index.js
@@ -1,3 +1,9 @@
-const item = document.getElementsByTagName("p");
+/**
+ * 1. Select the h1 element
+ * 2. Save the h1 element's text to a variable
+ * 3. Change the paragraph element's text to be the same as the text inside the h1
+ */
 
-item[0].innerText = "▓ ▒ ░ ◣◥♬⎈⌫";
+var item = document.querySelector("h1").textContent;
+
+document.querySelector("p").textContent = item + " [grabbed from <h1>]";
