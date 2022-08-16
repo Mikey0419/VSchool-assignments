84663e2e1836f0f546b071990b8ae7590e7ae5fb
Michael Hardin <hardin0419@gmail.com>
2022-08-16 08:16:48 -0600

    Updated index.html, index.js in For-loops-and-createElement directory

--- assignments/Level-1/DOM/Creating-Elements-JS/For-loops-and-createElement/index.js
+++ assignments/Level-1/DOM/Creating-Elements-JS/For-loops-and-createElement/index.js
@@ -1,16 +1,17 @@
 var powerRangers = [
-    "Jason Lee Scott", 
-    "Kimberly Hart", 
-    "Zack Taylor", 
-    "Trini Kwan", 
-    "Billy Cranston"
+    {name: "Jason Lee Scott", color: "Red"}, 
+    {name: "Kimberly Hart", color: "Pink"}, 
+    {name: "Zack Taylor", color: "Black"}, 
+    {name: "Trini Kwan", color: "Yellow"}, 
+    {name: "Billy Cranston", color: "Blue"},
+    {name: "Tommy Oliver", color: "Green"}
 ]
 
 var list = document.querySelector("#orderedList");
 
 for (var i = 0; i < powerRangers.length; i++) {
    var tempListItem = document.createElement("li");
-   tempListItem.textContent = powerRangers[i];
+   tempListItem.textContent = powerRangers[i].name + "  [" + powerRangers[i].color + " ranger]";
    console.log("adding ", tempListItem);
    list.append(tempListItem);
 }
