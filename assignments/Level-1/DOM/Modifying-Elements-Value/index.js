c165f0db33aefb2ee2b1b45d23bbcf4225cf77f1
Michael Hardin <hardin0419@gmail.com>
2022-08-16 11:37:29 -0600

    Modified index.html, index.js in Modifying-Elements-Value directory

--- assignments/Level-1/DOM/Modifying-Elements-Value/index.js
+++ assignments/Level-1/DOM/Modifying-Elements-Value/index.js
@@ -1,6 +1,10 @@
 var inputValue = document.querySelector("#text-input");
+var content = document.getElementById("content");
 
 inputValue.addEventListener("keypress", function(e) {
     //log the value if the 'Enter' button is pressed
-    if(e.keyCode == 13) { console.log(inputValue.value); }
+    if(e.keyCode == 13) {
+        console.log(inputValue.value);
+        content.innerHTML += inputValue.value + "<br>";
+    }
 })
