e83f52ea9bd41c045e359dd6f9d4c0d52b3a9d42
Michael Hardin <hardin0419@gmail.com>
2022-08-22 10:01:21 -0600

    Updated index.js to append element to body

--- assignments/Level-1/Forms/Forms-part-1/index.js
+++ assignments/Level-1/Forms/Forms-part-1/index.js
@@ -6,5 +6,11 @@ form.addEventListener("submit", e => {
     e.preventDefault();
     console.log(form["text-value"].value);
 
+    // create element, assign input value, and append to body
+
+    const myParagraph = document.createElement("p");
+    myParagraph.textContent = form["text-value"].value;
+    document.getElementsByTagName("body")[0].append(myParagraph);
+
     form["text-value"].value = "";
 })
