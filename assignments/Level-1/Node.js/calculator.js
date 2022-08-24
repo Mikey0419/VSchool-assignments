733713b417ec9e2b3e945b8532385e53dfcbd8a6
Michael Hardin <hardin0419@gmail.com>
2022-08-24 10:04:29 -0600

    Updated calculator.js

--- assignments/Level-1/Node.js/calculator.js
+++ assignments/Level-1/Node.js/calculator.js
@@ -29,3 +29,22 @@ const divideTwo = (num1, num2) => {
 const subtractTwo = (num1, num2) => {
     return num1 - num2;
 }
+
+options = ['Add', 'Subtract', 'Multiply', 'Divide'];
+
+var firstNumber = readLine.questionInt('Please enter the first number: ');
+var secondNumber = readLine.questionInt('Please enter the second number: ');
+
+index = readLine.keyInSelect(options, 'Please enter the operation to perform: ');
+
+if (options[index] == 'Add') {
+    console.log("\nThe result is: ", addTwo(firstNumber, secondNumber));
+} else if (options[index] == 'Subtract') {
+    console.log("\nThe result is: ", subtractTwo(firstNumber, secondNumber));
+} else if (options[index] == 'Multiply') {
+    console.log("\nThe result is: ", multiplyTwo(firstNumber, secondNumber));
+} else if (options[index] == 'Divide') {
+    console.log("\nThe result is: ", divideTwo(firstNumber, secondNumber));
+} else { 
+    console.log("\nGoodbye!");
+ }
