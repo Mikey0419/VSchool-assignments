219a093874db649b75da8bd4da3afc7a5d77a8e4
Michael Hardin <hardin0419@gmail.com>
2022-08-30 14:22:46 -0600

    Added index.js

--- assignments/Level-1/JavaScript/Array-Methods/index.js
+++ assignments/Level-1/JavaScript/Array-Methods/index.js
@@ -6,21 +6,38 @@ var arrOfFood = ["pancake", "egg", "ice cream", "pizza", "hamburger"]
 
 // push - adds item(s) to the end of the array
 
+arrOfFood.push("sushi");
+console.log(arrOfFood);
+
 // pop - removes item from the end of the array
+
+arrOfFood.pop("sushi");
+console.log(arrOfFood);
 
 // unshift - adds item(s) to the beginning of the array
 
+arrOfFood.unshift("coke zero", "biscuit");
+console.log(arrOfFood);
+
 // shift - removes item from the beginning of the array
+
+arrOfFood.shift('coke zero');
+console.log(arrOfFood);
 
 // concat
 
 // indexOf
+
+console.log(arrOfFood.indexOf('ice cream'));
 
 // slice
 
 // join
 
 // reverse
+
+arrOfFood.reverse();
+console.log(arrOfFood);
 
 // splice
 
