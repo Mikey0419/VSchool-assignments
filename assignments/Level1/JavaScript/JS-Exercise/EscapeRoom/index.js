9da75c328622963a307d41186545f186cd23fcc2
Michael Hardin <hardin0419@gmail.com>
2022-09-03 20:28:19 -0600

    Updated index.js

--- assignments/Level1/JavaScript/JS-Exercise/EscapeRoom/index.js
+++ assignments/Level1/JavaScript/JS-Exercise/EscapeRoom/index.js
@@ -14,15 +14,15 @@ while (isAlive) {
     
     switch (index) {
         //do something here with case
-        case -1:
+        case -1: //user chooses [0] CANCEL
             isAlive = false;
             break;
-        case 0:
+        case 0: //user chooses [1] 🕳 Put hand in hole
             console.clear();
             console.log("\t\tOh no! 💀💀💀 I mean, I did warn you ...\n\n");
             isAlive = false;
             break;
-        case 1:
+        case 1: //user chooses [2] 🔑 Find the key
             if (hasKey === false) {
                 hasKey = true;
                 console.clear();
@@ -32,7 +32,7 @@ while (isAlive) {
                 console.log("\t\tYou've already found the the 🔑 ... open the 🚪 and be free ....\n\n");
             }
             break;
-        case 2:
+        case 2: //user chooses [3] 🚪 Open the door
             console.clear();
             (hasKey === true) ? (console.log("\t\tYou did it! 🥳🎈🎊🎁🍾🎉"), isAlive = false) : console.log("\t\tYou don't have the 🔑.  Find the 🔑 then open the 🚪 ...\n\n");
             break;
