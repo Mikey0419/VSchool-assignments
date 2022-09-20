8820765df419deaea291ef3cb5fc76139bced27a
Michael Hardin <hardin0419@gmail.com>
2022-09-20 08:18:28 -0600

    Added sound

--- assignments/Level2/assignments/MarioPestControl/index.js
+++ assignments/Level2/assignments/MarioPestControl/index.js
@@ -5,6 +5,23 @@ const bobElement = document.getElementById("bobomb");
 const goombaElement = document.getElementById("goomba");
 const cheepElement = document.getElementById("cheep");
 
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+}
+
+const coinSound = new sound("./coin.mp3");
+
 const updateTotal = () => {
     let subTotal = 0;
     let bobOmbTotal = (isNaN(bobElement.value)) ? 0 : bobElement.value;
@@ -13,6 +30,7 @@ const updateTotal = () => {
 
     subTotal = new Number(bobOmbTotal * 7 + goombaTotal * 5 + cheepTotal * 11);
 
+    coinSound.play();
     return subTotal;
 }
 
