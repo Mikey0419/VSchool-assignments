66263071dfbb1f6c938f25c260ee20d8e502de8a
Michael Hardin <hardin0419@gmail.com>
2022-09-21 15:07:03 -0600

    Removed index.html. Updated index.js

--- assignments/Level2/lessons/HigherOrderFunctions/index.js
+++ assignments/Level2/lessons/HigherOrderFunctions/index.js
@@ -65,3 +65,30 @@ const userNames = users.map(function(x) {
 */
 
 console.log("\n", userNames);
+
+// .filter()
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const result = arr.filter(function(num){
+//     if(num % 2 === 0){
+//         return num + 10
+//     }
+// })
+
+// const result = arr.filter(num => num % 2 === 0)
+
+// console.log(result)
+
+
+
+const movies = [
+    { name: "movie1", genre: "horror" },
+    { name: "movie2", genre: "action" },
+    { name: "movie3", genre: "action" },
+    { name: "movie4", genre: "fantasy" }
+]
+
+const action = movies.filter(actionMovie => actionMovie.genre === "action");
+
+console.log(action);
