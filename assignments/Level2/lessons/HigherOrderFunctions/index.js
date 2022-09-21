9313edeb6aec9c7f3ddbc0d13e508c4f6832152b
Michael Hardin <hardin0419@gmail.com>
2022-09-21 10:01:16 -0600

    Updated index.js with map function

--- assignments/Level2/lessons/HigherOrderFunctions/index.js
+++ assignments/Level2/lessons/HigherOrderFunctions/index.js
@@ -25,3 +25,43 @@ const multiply = (num1, num2) => {
 }
 
 console.log(doMath(5, 4, multiply));
+
+// Higher order array methods
+// .map()
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+const result = arr.map(function(num){ // arr[i]
+    return num + 10;
+})
+
+/*
+ES6 version of the above map
+
+const result = arr.map(num => num + 10);
+*/
+
+console.log("arr = ", arr);
+console.log("\n\narr has been mapped into result:\n", result);
+
+const users = [
+    { name: "joe" },
+    { name: "steve" },
+    { name: "lisa" }
+]
+
+/* Create a new array of user names */
+
+const userNames = users.map(x => x.name);
+
+/*
+
+Same as:
+
+const userNames = users.map(function(x) {
+    return x.name;
+})
+
+*/
+
+console.log("\n", userNames);
