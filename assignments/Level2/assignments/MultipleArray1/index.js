9d30c54859ec2c178309ff9da9416c5ab84c906d
Michael Hardin <hardin0419@gmail.com>
2022-09-23 13:25:42 -0600

    Finished Multiple Array Methods exercise

---
+++ assignments/Level2/assignments/MultipleArray1/index.js
@@ -0,0 +1,76 @@
+let peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+let finalArr = [];
+
+/*
+
+
+1. Returns a list of everyone older than 18, which is
+2. sorted alphabetically by last name, and where
+3. each name and age is embedded in a string that looks like an HTML `<li>` element.
+
+*/
+
+function sortedOfAge(arr){
+    //must be over 18
+    //arr.filter grabs all over 18
+    const resultOfAge = arr.filter(item => item.age >= 18);
+
+    const alphaByLastName = resultOfAge.sort((a, b) => (a.lastName > b.lastName) ? 1 : -1);
+
+    const finalList = alphaByLastName.map(item => `<li>${item.firstName} ${item.lastName} is ${item.age}</li>`);
+
+    return finalList;
+    
+}
+
+console.log(sortedOfAge(peopleArray));
+
+/*
+Output:
+[
+    "<li>Kyle Mooney is 27</li>",
+    "<li>Sarah Palin is 47</li>",
+    "<li>Rick Sanchez is 78</li>",
+    "<li>Morty Smith is 29</li>",
+    "<li>Lev Tolstoy is 82</li>"
+]
+*/
+
+
