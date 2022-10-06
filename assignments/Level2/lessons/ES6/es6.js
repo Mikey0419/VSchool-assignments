03d476411b1d989c8814c36cd99e9d8bd88d436d
Michael Hardin <hardin0419@gmail.com>
2022-10-06 09:15:16 -0600

    Adjustments made

---
+++ assignments/Level2/lessons/ES6/es6.js
@@ -0,0 +1,33 @@
+// Why is ageRange not defined?
+// How can we fix this function with var? 
+// How can we fix this function avoiding the use of var?
+// Why would you use const vs let?
+function getAgeRange({age}){
+    let ageRange
+    if(age < 18){
+        ageRange = "Child"
+    } else if(age >= 18 && age < 80){
+        ageRange = "Adult"
+    } else {
+        ageRange = "Elderly Person"
+    }
+
+    return ageRange
+}
+// What would the above function potentially look like if we destructured the person parameter?
+
+const person = { name: "Andrea", age: 27 }
+// Why can we add .ageRange to person when we used const to define person?
+person.ageRange = getAgeRange(person) // feel free to comment out lines that cause errors
+person.dog = getAgeRange(person);
+//or lines that clutter the console
+console.log(person, '\n');
+console.log(`The ${person.ageRange}, ${person.name}, is ${person.age} years old.`)
+// change the above concatented string into a template literal
+
+const springMonths = ["March", "April", "May"]
+const fallMonths = ["September", "October", "November"]
+// replace this line with one that uses a spread oporator to do the same thing as concat
+const fringeMonths = [...springMonths, ...fallMonths]
+
+// console.log(springMonths, fallMonths, fringeMonths)
