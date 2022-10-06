221c5c40720693ed71c11b1d00513c2b6dfd34d6
Michael Hardin <hardin0419@gmail.com>
2022-10-06 09:32:20 -0600

    Completed ES6 lessons 1 & 2

--- assignments/Level2/lessons/ES6/es6.js
+++ assignments/Level2/lessons/ES6/es6.js
@@ -31,3 +31,71 @@ const fallMonths = ["September", "October", "November"]
 const fringeMonths = [...springMonths, ...fallMonths]
 
 // console.log(springMonths, fallMonths, fringeMonths)
+
+/*
+
+PART 2
+
+*/
+
+/* https://coursework.vschool.io/es6-practice-let-const-fat-arrow-functions-default-arguments-template-literals/
+https://coursework.vschool.io/es6-practice-object-literals-destructuring-rest-and-spread-operators/ */
+
+/* For this review, follow the directions in the JS file to make the code more ES6y. Comment and uncomment code as you work down the file. Run it to make these functions work or just more readable practicing ES6.
+
+You'll be using:
+
+arrow functions
+rest
+spread
+destructuring in a parameter
+default parameters */
+
+const myButton = document.getElementById("mybutton");
+const output = document.getElementById('output');
+const input = document.getElementById('input');
+// change this function to be an arrow function
+/* myButton.addEventListener("click", () => { output.innerText = "You've submitted: " + input.value;
+}) */
+
+myButton.addEventListener("click", () => {
+    output.innerText = "You have submitted: " + input.value;
+})
+
+//-------------------------------------------------------------------------------------------
+// Use the Rest Operator to help this function return an array of animals, no matter how many animals are passed to it:
+function collectAnimals(...arrAnimals) {  
+    console.log(arrAnimals);
+}
+
+collectAnimals("dog", "cat", "mouse", "jackolope", "platypus"); 
+// ["dog", "cat", "mouse", "jackolope", "platypus"]
+
+//-------------------------------------------------------------------------------------------
+// Use destructuring to use the property names as variables:
+
+const vacation = {  
+  location: "Burly Idaho",
+  duration: "2 weeks"
+};
+
+function parseSentence({location, duration}){
+  return `We're going to have a good time in ${location} for ${duration}`
+}
+
+console.log(parseSentence(vacation));
+
+// console.log(parseSentence(vacation)) // this line will break stuff unless you fix the parameter
+
+//-------------------------------------------------------------------------------------------
+// Make the following function more ES6xy. Use at least both the rest and spread operators:
+// this function should add as many items to the front of the returned array as the caller of the function wants
+const unshift = (array, ...arr) => {  return [...arr, ...array] }
+
+//-------------------------------------------------------------------------------------------
+// make "greeting" default to "hello"
+function greet(name, greeting = "Hello"){
+    return greeting + ", " + name
+}
+
+console.log(greet('Emily'))
