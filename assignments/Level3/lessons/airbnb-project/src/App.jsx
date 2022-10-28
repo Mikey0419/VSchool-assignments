3a4053e42009d5fd571db3cf64c23698d5fda65e
Michael Hardin <hardin0419@gmail.com>
2022-10-28 17:01:21 -0600

    Updated App and Card to use mapped data from data.jsx file

--- assignments/Level3/lessons/airbnb-project/src/App.jsx
+++ assignments/Level3/lessons/airbnb-project/src/App.jsx
@@ -1,30 +1,8 @@
 import Hero from './components/Hero.jsx';
 import Navbar from './components/Navbar.jsx';
 import Card from './components/Card.jsx';
+import data from './data.jsx';
 import './App.css';
-
-const gridData = [
-  {
-    id: 1,
-    imgUrl: "./src/assets/swimmer.png",
-    available: "SOLD OUT",
-    rating: "5.0",
-    reviews: 6,
-    title: "Life lessons with Katie Zaferes",
-    price: 136,
-    country: "USA"
-  },
-  {
-    id: 2,
-    imgUrl: "./src/assets/swimmer.png",
-    available: "SOLD OUT",
-    rating: "5.0",
-    reviews: 6,
-    title: "Life lessons with Katie Zaferes",
-    price: 136,
-    country: "USA"
-  }
-]
 
 /*
 <Card
@@ -39,10 +17,20 @@ const gridData = [
 */
 
 function App() {
+  const cardElement = data.map(item => {
 
-  const cardElement = gridData.map(item => {
+    function available() {
+      if (item.openSpots === 0) {
+        return "SOLD OUT" 
+      } else {
+          return `${item.openSpots} SPOTS`
+        }
+    }
+
+    let spots = available();
+    
     return (
-      <Card key={item.id} imgUrl={item.imgUrl} available={item.available} rating={item.rating} reviews={item.reviews} title={item.title} price={item.price} country={item.country} />
+      <Card key={item.id} coverImg={item.coverImg} available={spots} rating={item.stats.rating} reviewCount={item.stats.reviewCount} title={item.title} price={item.price} location={item.location} />
     )
   })
 
--- assignments/Level3/lessons/airbnb-project/src/components/Card.jsx
+++ assignments/Level3/lessons/airbnb-project/src/components/Card.jsx
@@ -5,14 +5,14 @@ function Card(props) {
     return(
         <div className="card-container">
             <div className="image-container">
-                <img className="card-image" src={props.imgUrl}></img>
+                <img className="card-image" src={props.coverImg}></img>
                 <p className="image-tag">{props.available}</p>
             </div>
             <div className="star-rating-container">
                 <img className="star" src={star}></img>
                 <p className="rating">{props.rating}&nbsp;</p>
-                <p className="reviews">{props.reviews}</p>
-                <p className="country">&nbsp;{props.country}</p>
+                <p className="reviews">{props.reviewCount}</p>
+                <p className="country">&nbsp;{props.location}</p>
             </div>
             <p className="description">{props.title}</p>
             <p className="pricing"><span className="bold">From ${props.price}</span> / person</p>
