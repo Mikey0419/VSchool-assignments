baf19e8877b519ae571ea3c7224d6e9a020e66c2
Michael Hardin <hardin0419@gmail.com>
2022-10-31 15:08:35 -0600

    Updated App and Card components: able to pass object as prop

--- assignments/Level3/lessons/airbnb-project/src/components/Card.jsx
+++ assignments/Level3/lessons/airbnb-project/src/components/Card.jsx
@@ -5,17 +5,17 @@ function Card(props) {
     return(
         <div className="card-container">
             <div className="image-container">
-                <img className="card-image" alt={props.description} src={props.coverImg}></img>
+                <img className="card-image" alt={props.item.description} src={props.item.coverImg}></img>
                 <p className="image-tag">{props.available}</p>
             </div>
             <div className="star-rating-container">
                 <img className="star" src={star}></img>
-                <p className="rating">{props.rating}&nbsp;</p>
-                <p className="reviews">{props.reviewCount}</p>
-                <p className="country">&nbsp;{props.location}</p>
+                <p className="rating">{props.item.stats.rating}&nbsp;</p>
+                <p className="reviews">{props.item.stats.reviewCount}</p>
+                <p className="country">&nbsp;{props.item.location}</p>
             </div>
-            <p className="description">{props.title}</p>
-            <p className="pricing"><span className="bold">From ${props.price}</span> / person</p>
+            <p className="description">{props.item.title}</p>
+            <p className="pricing"><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
