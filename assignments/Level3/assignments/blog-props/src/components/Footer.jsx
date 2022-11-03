6f21295a6f679a59dba5eb6ef07343aff757c9c3
Michael Hardin <hardin0419@gmail.com>
2022-11-03 16:21:37 -0600

    Added Footer styling

--- assignments/Level3/assignments/blog-props/src/components/Footer.jsx
+++ assignments/Level3/assignments/blog-props/src/components/Footer.jsx
@@ -2,7 +2,15 @@ import './footer.css'
 
 function Footer() {
     return(
-        <p>Footer stuff</p>
+        <div className="footer">
+            <div className="icon-container">
+                <ul className="list-icons">
+                    <li><span className="icon icon-twitter"></span></li>
+                    <li><span className="icon icon-facebook"></span></li>
+                    <li><span className="icon icon-github"></span></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
