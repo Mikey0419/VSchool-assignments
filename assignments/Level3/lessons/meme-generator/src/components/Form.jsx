f85029e3e0480c07e5baf1b2c18e025ec72fa98b
Michael Hardin <hardin0419@gmail.com>
2022-11-21 13:38:40 -0700

    Updated Form component and styling

--- assignments/Level3/lessons/meme-generator/src/components/Form.jsx
+++ assignments/Level3/lessons/meme-generator/src/components/Form.jsx
@@ -1,22 +1,27 @@
+import React from 'react'
 import './form.css'
+import data from '../data'
 
 function Form() {
+    const [image, setImage] = React.useState("", []);
 
-    function handleSubmit(e) {
+    function randomMeme(e) {
         e.preventDefault();
-        console.log(myform.first.value, myform.second.value);
-        myform.first.value = ''; myform.second.value = '';
-        myform.first.focus();
+        const memeArray = data.data.memes;
+        const randomNumber = Math.floor(Math.random() * (memeArray.length - 1));
+
+        console.log(`[${randomNumber}]`, memeArray[randomNumber].url);
+        setImage(memeArray[randomNumber].url);
     }
 
     return(
-        <form name="myform" onSubmit={handleSubmit} className="container">
+        <form onSubmit={randomMeme} name="myform" className="container">
             <div className="field-container">
                 <input name="first" type="text" />
                 <input name="second" type="text" />
             </div>
             <button id="meme-button" className="submit">Get a new meme image 🖼</button>
-            <img className="meme-image" src="" />
+            <img className="meme-image" src={image} />
         </form>
     )
 }
