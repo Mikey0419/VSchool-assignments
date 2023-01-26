ebf0daec868ba00a7ed5113edb28622ae027aa3d
Michael Hardin <hardin0419@gmail.com>
2023-01-26 13:11:08 -0700

    Axios added. Troubleshooting constant GET request.

--- assignments/Level4/assignments/ugly-things/src/List.jsx
+++ assignments/Level4/assignments/ugly-things/src/List.jsx
@@ -1,14 +1,28 @@
 import { useContext } from 'react'
 import { Context } from './context'
+import './list.css'
+
+const testImg = 'https://media.istockphoto.com/id/479669772/photo/small-town-lights.webp?s=2048x2048&w=is&k=20&c=PDn3jq5Qh6SOhmVybmesNDOiX1YbF0Vm2JQEmgHF6Mo='
 
 function List(props) {
     
-    const {list} = useContext(Context)
+    const {list, addItem, deleteItem} = useContext(Context)
 
-    console.log(list)
+    const el = list.map((item, index) => {
+        return(
+            <div key={index}>
+                <p><b>{item.title}</b></p>
+                <p><i>{item.description}</i></p>
+                {item._id && <button onClick={() => deleteItem(item._id)}>Delete</button>}
+                <img width="175px" height="175px" src={item.imgUrl} />
+            </div>
+        )
+    })
 
     return(
         <>
+            {el}
+            <button onClick={() => addItem('test', 'test test test', testImg)}>Add</button>
         </>
     )
 }
--- assignments/Level4/assignments/ugly-things/src/context.jsx
+++ assignments/Level4/assignments/ugly-things/src/context.jsx
@@ -1,32 +1,52 @@
 import { useState, useEffect, createContext } from 'react'
+import axios from 'axios'
 
 const Context = createContext()
 
 const ContextProvider = (props) => {
 
-    const [list, setList] = useState([
-        {
-            title: "Test 1",
-            description: "Test description 1",
-            imgUrl: "https://www.example.com/img1.jpeg"
-        },
-        {
-            title: "Test 2",
-            description: "Test description 2",
-            imgUrl: "https://www.example.com/img2.jpeg"
-        }
-    ])
+    const [list, setList] = useState([])
 
     useEffect(() => {
-        fetch("https://api.vschool.io/michaelhardin/thing")
-            .then(res => res.json())
-            .then(data => setList(data.data))
-    }, [])
+        axios.get("https://api.vschool.io/michaelhardin/thing")
+            .then(res => {
+                console.log(res.data)
+                setList(res.data)
+            })
+    }, [list])
+
+    function addItem(itemTitle, desc, url) {
+
+        const newItem = {
+            title: itemTitle,
+            description: desc,
+            imgUrl: url
+        }
+
+
+        axios.post("https://api.vschool.io/michaelhardin/thing", newItem)
+            .then(res => {
+                console.log(res.data)
+                setList(prev => [...prev, newItem])
+            })
+    }
+
+    function deleteItem(itemId) {
+        axios.delete(`https://api.vschool.io/michaelhardin/thing/${itemId}`)
+            .then(res => {
+                console.log(res.data)
+                setList(
+                    list.filter(i => i.id === itemId)
+                )
+            })
+    }
 
     return(
         <Context.Provider
             value={{
-                list
+                list,
+                addItem,
+                deleteItem
             }}>
             {props.children}
         </Context.Provider>
