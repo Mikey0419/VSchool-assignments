b38f996e770e5d18908b4a2f2d5543acdc70790c
Michael Hardin <hardin0419@gmail.com>
2023-01-27 10:04:01 -0700

    Added form.

--- assignments/Level4/assignments/ugly-things/src/List.jsx
+++ assignments/Level4/assignments/ugly-things/src/List.jsx
@@ -1,12 +1,20 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Context } from './context'
 import './list.css'
 
-const testImg = 'https://media.istockphoto.com/id/479669772/photo/small-town-lights.webp?s=2048x2048&w=is&k=20&c=PDn3jq5Qh6SOhmVybmesNDOiX1YbF0Vm2JQEmgHF6Mo='
+const defaultImage = "https://i0.wp.com/shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png?w=1738&ssl=1";
 
 function List(props) {
     
     const {list, addItem, deleteItem} = useContext(Context)
+
+    const [formData, setFormData] = useState(
+        {
+            title: '',
+            description: '',
+            url: ''
+        }
+    )
 
     const el = list.map((item, index) => {
         return(
@@ -14,16 +22,72 @@ function List(props) {
                 <p><b>{item.title}</b></p>
                 <p><i>{item.description}</i></p>
                 <p><u>{item._id}</u></p>
-                <button onClick={() => deleteItem(item._id)}>Delete</button>
-                <img width="175px" height="175px" src={item.imgUrl} />
+                <img width="250px" height="175px" src={item.imgUrl} />
+                <br />
+                <button className='delete' onClick={() => deleteItem(item._id)}>Delete</button>
             </div>
         )
     })
 
+    function handleChange(event) {
+        event.preventDefault();
+
+        const {name, value} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
+    function clearForm() {
+        let data = {
+            title: '',
+            description: '',
+            url: ''
+        }
+
+        setFormData(data);
+    }
+
     return(
         <>
             {el.length > 0 && el}
-            <button onClick={() => addItem('test', 'test test test', testImg)}>Add</button>
+            <br />
+            <form className="form">
+                <input
+                    type="text"
+                    placeholder="Title"
+                    onChange={handleChange}
+                    name="title"
+                    value={formData.title}
+                    required
+                />
+                <input
+                    type="text"
+                    placeholder="Description"
+                    onChange={handleChange}
+                    name="description"
+                    value={formData.description}
+                />
+                <input
+                    type="text"
+                    placeholder="Image URL"
+                    onChange={handleChange}
+                    name="url"
+                    value={formData.url}
+                />
+                <button
+                    className="create"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        addItem(formData.title === '' ? 'Title' : formData.title, formData.description === '' ? 'Description' : formData.description, formData.url === '' ? defaultImage : formData.url);
+                        clearForm();
+                    }}>
+                        Add
+                </button>
+            </form>
         </>
     )
 }
