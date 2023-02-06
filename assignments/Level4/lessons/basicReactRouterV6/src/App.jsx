7b1ad7ffd4d846c6db72b38eaa64e06cad81bcc2
Michael Hardin <hardin0419@gmail.com>
2023-02-06 09:37:33 -0700

    Adding Links.

--- assignments/Level4/lessons/basicReactRouterV6/src/App.jsx
+++ assignments/Level4/lessons/basicReactRouterV6/src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import About from './About'
+import Home from './Home'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
       <Router>
+
+        <nav style={{padding: "25px"}}>
+          <Link to = "/">
+            Home
+          </Link>
+          <Link to = "/about">
+            About
+          </Link>
+        </nav>
+
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
---
+++ assignments/Level4/lessons/basicReactRouterV6/src/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return(
+        <div className="home-container">
+            <p>Home</p>
+            <p>This is the Home section.</p>
+        </div>
+    )
+}
