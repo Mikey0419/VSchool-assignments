a478dc96bb7ce8acec44a521abe2191c086f9c29
Michael Hardin <hardin0419@gmail.com>
2023-02-07 13:57:28 -0700

    Updated Services component with styling.

--- assignments/Level4/lessons/basicReactRouterV6/src/Services.jsx
+++ assignments/Level4/lessons/basicReactRouterV6/src/Services.jsx
@@ -1,8 +1,29 @@
+import Wrench from './assets/wrench.png'
+
 export default function Services() {
     return(
         <div className="home-container">
-            <p>Services</p>
-            <p>This is the Services section.</p>
+            <div className="card-container">
+                <div className="card">
+                    <img className="wrench" src={Wrench} />
+                    <p>Services 1</p>
+                    <p>This is the Services section.</p>
+                </div>
+            </div>
+            <div className="card-container">
+                <div className="card">
+                    <img className="wrench" src={Wrench} />
+                    <p>Services 1</p>
+                    <p>This is the Services section.</p>
+                </div>
+            </div>
+            <div className="card-container">
+                <div className="card">
+                    <img className="wrench" src={Wrench} />
+                    <p>Services 1</p>
+                    <p>This is the Services section.</p>
+                </div>
+            </div>
         </div>
     )
 }
